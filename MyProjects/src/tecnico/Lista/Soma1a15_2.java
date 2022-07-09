5e67c0e5b5bf959ca2bd31a8dc4b9c57c6140ac0
Isabel Coutinho <isabelcoutinhors@gmail.com>
2022-07-09 15:38:25 -0300

    commit

--- MyProjects/src/tecnico/Lista/Soma1a15_2.java
+++ MyProjects/src/tecnico/Lista/Soma1a15_2.java
@@ -1,6 +1,6 @@
 package tecnico.Lista;
 
-public class Soma1a152 {
+public class Soma1a15_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- MyProjects/src/tecnico/Lista/Soma1a15_3.java
+++ MyProjects/src/tecnico/Lista/Soma1a15_3.java
@@ -1,6 +1,6 @@
 package tecnico.Lista;
 
-public class Soma1a153 {
+public class Soma1a15_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
