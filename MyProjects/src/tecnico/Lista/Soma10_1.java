b9609d20cf4b91a5fdc24f6f0a957f00421b3dd7
Isabel Coutinho <isabelcoutinhors@gmail.com>
2022-07-11 13:26:16 -0300

    Lista

--- MyProjects/src/tecnico/Lista/Soma10_1.java
+++ MyProjects/src/tecnico/Lista/Soma10_1.java
@@ -5,13 +5,13 @@ public class Soma10_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		int i, numero, soma;
+		int i, num, soma;
 		i = 1;
 		soma = 0;
 		while (i<=15){ 
 		System.out.println("Digite um número: ");
-		numero = input.nextInt();
-		soma = soma + numero;
+		num = input.nextInt();
+		soma = soma + num;
 		i++;
 		}
 		System.out.println("A soma dos números digitados é: "+soma);
