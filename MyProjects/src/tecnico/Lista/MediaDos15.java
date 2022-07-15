c710c67130ead0c5675da23e7e43278af8174a5a
Isabel Coutinho <isabelcoutinhors@gmail.com>
2022-07-15 20:41:15 -0300

    Lista

--- MyProjects/src/tecnico/Lista/MediaDos15.java
+++ MyProjects/src/tecnico/Lista/MediaDos15.java
@@ -1,5 +1,17 @@
 package tecnico.Lista;
-
+import java.util.Scanner;
 public class MediaDos15 {
-
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int i, numero, soma;
+		double media;
+		soma = 0;
+		for (i=1; i<=15; i++){
+			System.out.println("Digite um número: ");
+			numero = input.nextInt();
+			soma = soma + numero;
+			}
+		media = soma/15.0;
+		System.out.println("A média dos números digitados é: "+media);
+		}
 }
