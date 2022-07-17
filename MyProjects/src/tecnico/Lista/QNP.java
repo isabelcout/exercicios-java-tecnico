c6afc9497edee7b59ee71b26f8f63764890c03bc
Isabel Coutinho <isabelcoutinhors@gmail.com>
2022-07-17 19:51:27 -0300

    Lista

---
+++ MyProjects/src/tecnico/Lista/QNP.java
@@ -0,0 +1,19 @@
+package tecnico.Lista;
+
+import java.util.Scanner;
+
+public class QNP {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int i, numero, quantidade;
+		quantidade = 0;
+		for (i=1; i<=20; i++){
+			System.out.println("Digite um número: ");
+			numero = input.nextInt();
+			if (numero % 2 == 0){
+				quantidade++;
+				}
+			} 
+		System.out.println("Quantidade de números digitados que são pares: "+quantidade);
+	}
+}
